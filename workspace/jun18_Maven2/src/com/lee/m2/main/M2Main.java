58f8ce82780d8f26869fd1433dafcd44fa8a56cf
soonLee <cjswosuny01@naver.com>
2019-06-18 12:18:38 +0900

    Add .jar files by using Maven

---
+++ workspace/jun18_Maven2/src/com/lee/m2/main/M2Main.java
@@ -0,0 +1,10 @@
+package com.lee.m2.main;
+
+//Maven에서 한번이라도 다운받은 적 있는 .jar파일들은 pom.xml탭에서 바로 사용 가능.
+// Dependencies 탭 -> Add -> 원하는 ./jar검색 -> 추가
+
+public class M2Main {
+	public static void main(String[] args) {
+		
+	}
+}
