f395dfd12a0151342e327b87154619bc0060e6fa
soonLee <cjswosuny01@naver.com>
2019-06-18 12:28:08 +0900

    rebuild index Maven

--- workspace/jun18_Maven2/src/com/lee/m2/main/M2Main.java
+++ workspace/jun18_Maven2/src/com/lee/m2/main/M2Main.java
@@ -3,6 +3,9 @@ package com.lee.m2.main;
 //Maven에서 한번이라도 다운받은 적 있는 .jar파일들은 pom.xml탭에서 바로 사용 가능.
 // Dependencies 탭 -> Add -> 원하는 ./jar검색 -> 추가
 
+//다운로드한 목록이 나오지 않는 경우에는 새로고침을 해주어야 함.
+// window -> show view -> other -> maven repositories
+// Local repositories -> local repositroy 우클릭 -> rebuild index
 public class M2Main {
 	public static void main(String[] args) {
 		
